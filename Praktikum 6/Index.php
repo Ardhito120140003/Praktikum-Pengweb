2a19b67b81d4063a15158f79485009b497098d2b
Ardhito120140003 <102868601+Ardhito120140003@users.noreply.github.com>
2022-12-12 16:50:04 +0700

    Add Praktikum 6

---
+++ Praktikum 6/Index.php
@@ -0,0 +1,32 @@
+<?php 
+function ConvertToRoman($num){ 
+    $num = intval($num); 
+    $result = ''; 
+
+    $romanNumber_Array = array( 
+        'M'  => 1000, 
+        'CM' => 900, 
+        'D'  => 500, 
+        'CD' => 400, 
+        'C'  => 100, 
+        'XC' => 90, 
+        'L'  => 50, 
+        'XL' => 40, 
+        'X'  => 10, 
+        'IX' => 9, 
+        'V'  => 5, 
+        'IV' => 4, 
+        'I'  => 1); 
+
+    foreach ($romanNumber_Array as $roman => $number){ 
+        $matches = intval($num / $number); 
+        $result .= str_repeat($roman, $matches); 
+        $num = $num % $number; 
+    } 
+
+    return $result; 
+} 
+
+echo ConvertToRoman(1141); 
+echo " ";
+?>
